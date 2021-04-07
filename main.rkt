ed97e59e68ba6bebf1ae302c7537ef295480b906
Brett Saiki <bksaiki@gmail.com>
2021-04-07 09:56:17 -0700

    Fix nonffi

--- main.rkt
+++ main.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(require herbie/plugin math/bigfloat rival generic-flonum)
+(require herbie/plugin math/bigfloat math/base rival generic-flonum)
 
 (eprintf "Loading generic float support...\n")
 
@@ -11,6 +11,9 @@
 (define ((inv-comparator test) . args)
   (for/or ([left args] [right (cdr args)])
     (not (test left right))))
+
+(define (from-bigfloat bff)
+  (λ args (bigfloat->flonum (apply bff (map bf args)))))
 
 (define (sym-append . args)
   (string->symbol (apply string-append (map ~s args))))
@@ -40,7 +43,7 @@
 
     ; Operator
     (define (register-fl-operator! op op-name argc fl-impl bf-impl ival-impl
-                #:nonffi [nonffi-imlp #f] #:itype [itype #f] #:otype [otype #f])
+                                  [nonffi-imlp #f] #:itype [itype #f] #:otype [otype #f])
       (define nonffi* (if nonffi-imlp nonffi-imlp fl-impl))
       (define op-name* (sym-append op-name '.fl es '- nbits))
       (define info-dict
@@ -82,51 +85,53 @@
       (λ () +nan.bf)
       (λ () (mk-ival +nan.bf)))
 
-    (register-fl-operator! '- 'neg 1 (gfl-op es nbits gfl-) bf- ival-neg)
-    (register-fl-operator! '+ '+ 2 (gfl-op es nbits gfl+) bf+ ival-add)
-    (register-fl-operator! '- '- 2 (gfl-op es nbits gfl-) bf- ival-sub)
-    (register-fl-operator! '* '* 2 (gfl-op es nbits gfl*) bf* ival-mult)
-    (register-fl-operator! '/ '/ 2 (gfl-op es nbits gfl/) bf/ ival-div)
-    (register-fl-operator! 'sqrt 'sqrt 1 (gfl-op es nbits gflsqrt) bfsqrt ival-sqrt)
-    (register-fl-operator! 'cbrt 'cbrt 1 (gfl-op es nbits gflcbrt) bfcbrt ival-cbrt)
-    (register-fl-operator! 'abs 'abs 1 (gfl-op es nbits gflabs) bfabs ival-fabs)
+    (register-fl-operator! '- 'neg 1 (gfl-op es nbits gfl-) bf- ival-neg -)
+    (register-fl-operator! '+ '+ 2 (gfl-op es nbits gfl+) bf+ ival-add +)
+    (register-fl-operator! '- '- 2 (gfl-op es nbits gfl-) bf- ival-sub -)
+    (register-fl-operator! '* '* 2 (gfl-op es nbits gfl*) bf* ival-mult *)
+    (register-fl-operator! '/ '/ 2 (gfl-op es nbits gfl/) bf/ ival-div /)
+    (register-fl-operator! 'sqrt 'sqrt 1 (gfl-op es nbits gflsqrt) bfsqrt ival-sqrt sqrt)
+    (register-fl-operator! 'cbrt 'cbrt 1 (gfl-op es nbits gflcbrt) bfcbrt ival-cbrt (curryr expt 1/3))
+    (register-fl-operator! 'abs 'abs 1 (gfl-op es nbits gflabs) bfabs ival-fabs abs)
 
-    (register-fl-operator! 'log 'log 1 (gfl-op es nbits gfllog) bflog ival-log)
-    (register-fl-operator! 'log2 'log2 1 (gfl-op es nbits gfllog2) bflog2 ival-log2)
-    (register-fl-operator! 'log10 'log10 1 (gfl-op es nbits gfllog10) bflog10 ival-log10)
-    (register-fl-operator! 'log1p 'log1p 1 (gfl-op es nbits gfllog1p) bflog1p ival-log1p)
+    (register-fl-operator! 'log 'log 1 (gfl-op es nbits gfllog) bflog ival-log log)
+    (register-fl-operator! 'log2 'log2 1 (gfl-op es nbits gfllog2) bflog2 ival-log2 (curryr log 2))
+    (register-fl-operator! 'log10 'log10 1 (gfl-op es nbits gfllog10) bflog10 ival-log10 (curry log 10))
+    (register-fl-operator! 'log1p 'log1p 1 (gfl-op es nbits gfllog1p) bflog1p
+                           ival-log1p (from-bigfloat bflog1p))
 
-    (register-fl-operator! 'exp 'exp 1 (gfl-op es nbits gflexp) bfexp ival-exp)
-    (register-fl-operator! 'exp2 'exp2 1 (gfl-op es nbits gflexp2) bfexp2 ival-exp2)
-    (register-fl-operator! 'expm1 'expm1 1 (gfl-op es nbits gflexpm1) bfexpm1 ival-expm1)
-    (register-fl-operator! 'pow 'pow 2 (gfl-op es nbits gflexpt) bfexpt ival-pow)
+    (register-fl-operator! 'exp 'exp 1 (gfl-op es nbits gflexp) bfexp ival-exp exp)
+    (register-fl-operator! 'exp2 'exp2 1 (gfl-op es nbits gflexp2) bfexp2 ival-exp2 (curry expt 2))
+    (register-fl-operator! 'expm1 'expm1 1 (gfl-op es nbits gflexpm1) bfexpm1
+                           ival-expm1 (from-bigfloat bfexpm1))
+    (register-fl-operator! 'pow 'pow 2 (gfl-op es nbits gflexpt) bfexpt ival-pow expt)
 
-    (register-fl-operator! 'sin 'sin 1 (gfl-op es nbits gflsin) bfsin ival-sin)
-    (register-fl-operator! 'cos 'cos 1 (gfl-op es nbits gflcos) bfcos ival-cos)
-    (register-fl-operator! 'tan 'tan 1 (gfl-op es nbits gfltan) bftan ival-tan)
-    (register-fl-operator! 'asin 'asin 1 (gfl-op es nbits gflasin) bfasin ival-asin)
-    (register-fl-operator! 'acos 'acos 1 (gfl-op es nbits gflacos) bfacos ival-acos)
-    (register-fl-operator! 'atan 'atan 1 (gfl-op es nbits gflatan) bfatan ival-atan)
+    (register-fl-operator! 'sin 'sin 1 (gfl-op es nbits gflsin) bfsin ival-sin sin)
+    (register-fl-operator! 'cos 'cos 1 (gfl-op es nbits gflcos) bfcos ival-cos cos)
+    (register-fl-operator! 'tan 'tan 1 (gfl-op es nbits gfltan) bftan ival-tan tan)
+    (register-fl-operator! 'asin 'asin 1 (gfl-op es nbits gflasin) bfasin ival-asin asin)
+    (register-fl-operator! 'acos 'acos 1 (gfl-op es nbits gflacos) bfacos ival-acos acos)
+    (register-fl-operator! 'atan 'atan 1 (gfl-op es nbits gflatan) bfatan ival-atan atan)
 
-    (register-fl-operator! 'sinh 'sinh 1 (gfl-op es nbits gflsinh) bfsinh ival-sinh)
-    (register-fl-operator! 'cosh 'cosh 1 (gfl-op es nbits gflcosh) bfcosh ival-cosh)
-    (register-fl-operator! 'tanh 'tanh 1 (gfl-op es nbits gfltanh) bftanh ival-tanh)
-    (register-fl-operator! 'asinh 'asinh 1 (gfl-op es nbits gflasinh) bfasinh ival-asinh)
-    (register-fl-operator! 'acosh 'acosh 1 (gfl-op es nbits gflacosh) bfacosh ival-acosh)
-    (register-fl-operator! 'atanh 'atanh 1 (gfl-op es nbits gflatanh) bfatanh ival-atanh)
+    (register-fl-operator! 'sinh 'sinh 1 (gfl-op es nbits gflsinh) bfsinh ival-sinh sinh)
+    (register-fl-operator! 'cosh 'cosh 1 (gfl-op es nbits gflcosh) bfcosh ival-cosh cosh)
+    (register-fl-operator! 'tanh 'tanh 1 (gfl-op es nbits gfltanh) bftanh ival-tanh tanh)
+    (register-fl-operator! 'asinh 'asinh 1 (gfl-op es nbits gflasinh) bfasinh ival-asinh asinh)
+    (register-fl-operator! 'acosh 'acosh 1 (gfl-op es nbits gflacosh) bfacosh ival-acosh acosh)
+    (register-fl-operator! 'atanh 'atanh 1 (gfl-op es nbits gflatanh) bfatanh ival-atanh atanh)
 
     (register-fl-operator! '== '== 2 (comparator gfl=) (comparator bf=) (comparator ival-==)
-                           #:itype name #:otype 'bool) ; override number of arguments
-    (register-fl-operator! '!= '!= 2 (inv-comparator gfl=) (inv-comparator bf=) ival-!=
-                           #:itype name #:otype 'bool) ; override number of arguments
+                           (comparator =) #:itype name #:otype 'bool) ; override number of arguments
+    (register-fl-operator! '!= '!= 2 (inv-comparator gfl=) (inv-comparator bf=) (comparator ival-!=)
+                           (inv-comparator =) #:itype name #:otype 'bool) ; override number of arguments
     (register-fl-operator! '< '< 2 (comparator gfl<) (comparator bf<) (comparator ival-<)
-                           #:itype name #:otype 'bool) ; override number of arguments
+                           (comparator <) #:itype name #:otype 'bool) ; override number of arguments
     (register-fl-operator! '> '> 2 (comparator gfl>) (comparator bf>) (comparator ival->)
-                           #:itype name #:otype 'bool) ; override number of arguments
+                           (comparator >) #:itype name #:otype 'bool) ; override number of arguments
     (register-fl-operator! '<= '<= 2 (comparator gfl<=) (comparator bf<=) (comparator ival-<=)
-                           #:itype name #:otype 'bool) ; override number of arguments
+                           (comparator <=) #:itype name #:otype 'bool) ; override number of arguments
     (register-fl-operator! '>= '>= 2 (comparator gfl>=) (comparator bf>=) (comparator ival->=)
-                           #:itype name #:otype 'bool) ; override number of arguments
+                           (comparator >=) #:itype name #:otype 'bool) ; override number of arguments
 
     #t]
    [_ #f]))
